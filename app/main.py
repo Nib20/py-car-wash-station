8c4622ecc5bd59926e3e4d7c88a8fc7dc6a5c05b
Nadiia Bohdan <n.bogdan202@gmail.com>
2025-05-05 18:49:57 +0300

    fixes for code style

--- app/main.py
+++ app/main.py
@@ -23,10 +23,7 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(
-        self,
-        car_list: list[Car]
-    ) -> float:
+    def serve_cars(self, car_list: list[Car]) -> float:
         total_income = 0
         for car in car_list:
             if self.clean_power > car.clean_mark:
@@ -35,10 +32,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(total_income, 1)
 
-    def calculate_washing_price(
-        self,
-        car: Car
-    ) -> float:
+    def calculate_washing_price(self, car: Car) -> float:
         price = (
             car.comfort_class
             * (self.clean_power - car.clean_mark)
@@ -47,17 +41,11 @@ class CarWashStation:
         )
         return round(price, 1)
 
-    def wash_single_car(
-        self,
-        car: Car
-    ) -> None:
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(
-        self,
-        new_rate: float
-    ) -> None:
+    def rate_service(self, new_rate: float) -> None:
         total = self.average_rating * self.count_of_ratings
         total += new_rate
         self.count_of_ratings += 1
